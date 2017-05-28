81d987ab9f76b01cbf077c15f664653d6fc30710
Nekmo <contacto@nekmo.com>
2017-05-28 06:13:04 +0200

    Initial commit

---
+++ cinebot/services/base.py
@@ -0,0 +1,148 @@
+import datetime
+import re
+from collections import defaultdict
+
+from bs4 import BeautifulSoup
+from requests import Session
+
+
+FILM_OPTIONS = {
+    '3D': '3D',
+    'UHD': '4K',
+    'VOSE': 'SUB',
+}
+
+def remove_words(text, words):
+    pattern = re.compile("(" + '|'.join([re.escape(word) for word in words]) + ")", re.I)
+    return pattern.sub("", text)
+
+
+class TimesList(list):
+    def __init__(self, times):
+        times = sorted(times, key=lambda x: x.time)
+        super(TimesList, self).__init__(times)
+
+
+class FilmTimeBase(object):
+    def __init__(self, film, date, time, options=None, booking=None):
+        if isinstance(time, str):
+            time = datetime.datetime.strptime(time, '%H:%M').time()
+        self.film = film
+        self.date = date
+        self.time = time
+        self.options = options
+        self.booking = booking
+
+    def __str__(self):
+        text = '{}'.format(self.time)
+        if self.options:
+            text += ' ({})'.format(', '.join(self.options))
+        return text
+
+    def __repr__(self):
+        return '<Time {}>'.format(self)
+
+
+class FilmBase(object):
+    film_time_class = FilmTimeBase
+
+    def __init__(self, location, date, name, film_options=None):
+        self.location = location
+        self.date = date
+        self.name = name
+        self.film_options = film_options
+
+    def get_cover(self):
+        raise NotImplementedError
+
+    def get_times(self):
+        times = [self.film_time_class(self, self.date, time['time'], time.get('options'), time.get('booking'))
+                for time in self.get_times_data()]
+        return TimesList(times)
+
+    def get_times_data(self):
+        raise NotImplementedError
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return '<Film {}>'.format(self)
+
+
+class LocationBase(object):
+    film_class = FilmBase
+
+    def __init__(self, service: 'ServiceBase', location_id, name: str):
+        self.service = service
+        self.id = location_id
+        self.name = name
+
+    def get_films(self, date=None):
+        date = date or datetime.date.today()
+        billboard_data = self.get_films_data(date)
+        return [self.film_class(self, date, data['name'], data.get('film_options', [])) for data in billboard_data]
+
+    def get_films_data(self, date):
+        raise NotImplementedError
+
+    def join_films_options(self, data):
+        """Muchos cines tienen sus películas separadas según sus opciones (VOSE, UHD, 3D...).
+        Esta función se encarga de unir dichas películas.
+        :param data:  
+        :return: 
+        """
+        films = defaultdict(list)
+        for film in data:
+            film = self.set_film_options(film)
+            films[film['name']].append(film)
+        return [{'name': name, 'film_options': options} for name, options in films.items()]
+
+    def set_film_options(self, film):
+        film = dict(film)
+        name = remove_words(film['name'], [' - '])
+        options = []
+        for option in FILM_OPTIONS:
+            if option in name:
+                options.append(option)
+                name = name.replace(option, '')
+        name = re.sub(' +', ' ', name)
+        film['options'] = options
+        film['name'] = name.strip()
+        return film
+
+    def match_name(self, other_name: str):
+        return other_name.lower() in self.name.lower()
+
+    def __str__(self):
+        return '{} ({})'.format(self.name, self.id)
+
+    def __repr__(self):
+        return '<Location {}>'.format(self)
+
+
+class ServiceBase(object):
+    location_class = LocationBase
+
+    def __init__(self):
+        self.session = self.get_session()
+        self.locations = self.get_locations()
+
+    def get_session(self):
+        return Session()
+
+    def find_by_name(self, name):
+        for location in self.locations:
+            if location.match_name(name):
+                return location
+
+    def soup_req(self, url, method='GET', params=None, data=None, session=None):
+        session = session or self.session
+        req = session.request(method, url, params, data)
+        return req, BeautifulSoup(req.text, 'html.parser')
+
+    def get_locations(self):
+        return [self.location_class(self, data['id'], data['name']) for data in self.get_locations_data()]
+
+    def get_locations_data(self):
+        raise NotImplementedError
---
+++ cinebot/services/cinesur.py
@@ -0,0 +1,81 @@
+import datetime
+import re
+
+from bs4 import BeautifulSoup
+
+from cinebot.services.base import ServiceBase, LocationBase, FilmBase, remove_words
+
+URL = 'https://www.cinesur.com/'
+URL_LIST = '{}listado.php'.format(URL)
+AJAX_COVERS = '{}cargador_ajax/peliculas2.php'.format(URL)
+
+class CinesurFilm(FilmBase):
+
+    def get_cover(self):
+        for film_option in self.film_options:
+            if not film_option['options']:
+                # Es la versión sin modificaciones. Nos quedamos con esta.
+                break
+        else:
+            return None
+        # Nota: la imagen no puede descargarse si no tiene el referer.
+        return film_option.get('cover')
+
+    def get_times_data(self):
+        all_times = []
+        for film_option in self.film_options:
+            times = film_option['data'].select('.sin_estilo')
+            all_times.extend([dict(booking=time.attrs['href'], time=time.string, options=film_option['options'])
+                              for time in times])
+        return all_times
+
+
+class CinesurLocation(LocationBase):
+    film_class = CinesurFilm
+
+    def get_films_data(self, date):
+        req = self.service.session.request('GET', URL_LIST, params=dict(
+            id_cine=self.id, fecha_alta=date.strftime('%d/%m/%Y'), idp='',
+        ))
+        # Debo arreglar el html antes de pasárselo a BeautifulSoup porque es inválido
+        text = remove_words(req.text, ['<p >', '<p>', '</p>', '</form>', '</option>'])
+        text = re.sub('<(?:form|option)([^>]*)>', '', text)
+        soup = BeautifulSoup(text, 'html.parser')
+        films = soup.select('div.claro, div.oscuro')
+        films = [dict(name=data.find('a', 'titulo_peli'), data=data) for data in films]
+        films = [dict(film, name=film['name'].string, sheet_url=URL + film['name'].attrs['href'])
+                 for film in films if film['name']]
+        covers = self.covers_ajax(date)
+        films = [dict(film, cover=covers.get(film['sheet_url'])) for film in films]
+        films = self.join_films_options(films)
+        return films
+
+    def covers_ajax(self, date):
+        """La página de las portadas se carga por Ajax, y encima tiene dependencia por
+        Cookies. Por ello, creo una nueva sesión vacía y repito la petición. Tras ello,
+        cargo la primera página, e itero entre las páginas. No sé el número de páginas,
+        así que me limito a seguir mientras el botón "siguiente" esté habilitado. Para
+        relacionar la imagen con la película, utilizo su enlace, que es el mismo.
+        """
+        session = self.service.get_session()
+        session.get(URL_LIST,params=dict(
+            id_cine=self.id, fecha_alta=date.strftime('%d/%m/%Y'), idp='',
+        ))
+        params = {'id_cine': self.id}
+        covers = {}
+        for i in range(1, 10):  # 10: máximo de páginas para evitar bucle
+            req, soup = self.service.soup_req(AJAX_COVERS, params=params, session=session)
+            page_covers = [a for a in soup.select('.peli a') if not a.attrs.get('class')]
+            covers.update({URL + a.attrs['href']: URL + a.find('img').attrs['src'] for a in page_covers})
+            if not soup.select('a.siguiente')[0].attrs.get('href'):
+                # Última página
+                break
+            params = {'pagina': i + 1}
+        return covers
+
+class CinesurService(ServiceBase):
+    location_class = CinesurLocation
+
+    def get_locations_data(self):
+        req, soup = self.soup_req(URL)
+        return [dict(id=a.attrs['href'].split('=')[1], name=a.string[2:]) for a in soup.select('.ciu a')]
