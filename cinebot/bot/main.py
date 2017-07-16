4727d6a26f99a079c5386418d94e37ea2fcf1467
Nekmo <contacto@nekmo.com>
2017-07-17 01:48:20 +0200

    Requirements.

--- cinebot/bot/main.py
+++ cinebot/bot/main.py
@@ -1,6 +1,6 @@
 from cinebot.bot.plugins.cinemas import CinemasPlugin
 from cinebot.bot.plugins.days import DaysPlugin, SearchPlugin
-from telegram_bot import BotBase
+from telegram_bot.bot import BotBase
 
 
 class CineBot(BotBase):
